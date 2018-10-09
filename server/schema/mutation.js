64826c426371882983f9c4fbac2e37fa10ddee9a
avshalomp <avshi1987@gmail.com>
2018-10-10 11:27:34 +1300

    server - add GraphQL addBook Mutation

--- server/schema/mutation.js
+++ server/schema/mutation.js
@@ -1,10 +1,11 @@
 const graphql = require('graphql');
-const { AuthorType } = require('./types');
+const { AuthorType, BookType } = require('./types');
 const db = require('../models');
 
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID } = graphql;
 
+/* Mutation */
 const Mutation = new GraphQLObjectType({
     name: 'Mutation',
     fields: {
@@ -21,7 +22,24 @@ const Mutation = new GraphQLObjectType({
                 });
                 return author.save();
             }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: { type: GraphQLString },
+                genre: { type: GraphQLString },
+                authorId: { type: GraphQLID }
+            },
+            resolve(parent, args){
+                let book = db.Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                });
+                return book.save();
+            }
         }
+
     }
 });
 
