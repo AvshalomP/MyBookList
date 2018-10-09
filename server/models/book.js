5ed93c85a737f138e1f53188b366e2187a4e5ad9
avshalomp <avshi1987@gmail.com>
2018-10-09 23:50:05 +1300

    server - add Book and Author db schema

---
+++ server/models/book.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+        },
+        genre: {
+            type: String,
+        },
+        authorId: {
+            type: String,
+        },
+    }
+);
+
+module.exports = mongoose.model("Book", bookSchema);
--- server/models/index.js
+++ server/models/index.js
@@ -5,3 +5,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
     console.log("Connected to database")
 });
+
+
+module.exports.Book = require('./book');
+module.exports.Author = require('./author');
