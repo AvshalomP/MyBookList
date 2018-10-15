af52c6a50b77103e1f3701ed61241ef173deaaa8
avshalomp <avshi1987@gmail.com>
2018-10-15 15:37:52 +1300

    client - display fetched book details from db

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import { getBookQuery } from "../services/queries/index";
 import { graphql } from 'react-apollo';
 
+
 class BookDetails extends Component {
     render(){
-        const { data } = this.props;
-        console.log("data: ", data);
+        const { book } = this.props.data;
+        const details = !book ? (<div>No book selected...</div>)
+                              : (<div>
+                                      <h2>{book.name}</h2>
+                                      <p>Genre: {book.genre}</p>
+                                      <p>Author: {book.author.name}</p>
+                                      <label>Other books by {book.author.name}: </label>
+                                      <ul className="other-books">
+                                          {book.author.books.map( book => {
+                                              return <li key={book.id}>{book.name}</li>
+                                          })}
+                                      </ul>
+                                  </div>
+                                );
 
-        return(
-            <div>Book Details....</div>
+        return (
+            <div id="book-details">
+                { details }
+            </div>
+
         )
     }
 }
